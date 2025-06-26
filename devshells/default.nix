7dbdbb245038eb711e7b9a66ccb26d6502840f74
Tristan Daniël Maat <t.maat@famedly.com>
2025-06-26 19:05:28 +0800

    feat(rust-devshell): Add support for aarch64-darwin

--- devshells/default.nix
+++ devshells/default.nix
@@ -1,8 +1,9 @@
 { nixpkgs, ... }@inputs:
 let
-  # TODO: Someone should test if the devshells work with
-  # aarch64-darwin so we can add support for MacOS
-  systems = [ "x86_64-linux" ];
+  systems = [
+    "x86_64-linux"
+    "aarch64-darwin"
+  ];
 in
 nixpkgs.lib.genAttrs systems (system: {
   rust = import ./rust.nix (inputs // { inherit system; });
