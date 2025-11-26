8d01ba8675f023df895d432477239ecf46e017d0
Emelie Graven <e.graven@famedly.com>
2025-11-26 15:42:52 +0100

    feat: Add bundled kubeconfig

--- devshells/k8s/default.nix
+++ devshells/k8s/default.nix
@@ -15,4 +15,18 @@ pkgs.mkShell {
       kubetui
       kubernetes-helm
     ];
+
+  shellHook = ''
+    KUBE_TMP=$(mktemp -d)
+    cp ${./kubeconfig.yaml} "$KUBE_TMP/config"
+    chmod 600 "$KUBE_TMP/config"
+
+    if [ -n "$KUBECONFIG" ]; then
+      export KUBECONFIG="$KUBE_TMP/config:$KUBECONFIG"
+    elif [ -f "$HOME/.kube/config" ]; then
+      export KUBECONFIG="$KUBE_TMP/config:$HOME/.kube/config"
+    else
+      export KUBECONFIG="$KUBE_TMP/config"
+    fi
+  '';
 }
